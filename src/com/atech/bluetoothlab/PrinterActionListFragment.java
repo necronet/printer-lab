b5e85d84bcc2860cf078764601aca5f18f49a337
Jose Ayerdis <joseayerdis@gmail.com>
2013-10-31 14:38:49 -0600

    Handling click listener of each items

--- src/com/atech/bluetoothlab/PrinterActionListFragment.java
+++ src/com/atech/bluetoothlab/PrinterActionListFragment.java
@@ -5,23 +5,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class PrinterActionListFragment extends Fragment {
+public class PrinterActionListFragment extends Fragment implements OnItemClickListener{
 
-	private ArrayAdapter adapter; 
-	
+	private ArrayAdapter<String> adapter;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
+
 		View view = inflater.inflate(R.layout.action_list, container, false);
-		adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.actions));
-		
-		ListView list = (ListView)view.findViewById(R.id.list);
+		adapter = new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, getResources()
+						.getStringArray(R.array.actions));
+
+		ListView list = (ListView) view.findViewById(R.id.list);
 		list.setAdapter(adapter);
-		
+
+		list.setOnItemClickListener(this);
 		return view;
 	}
 
@@ -29,5 +34,19 @@ public class PrinterActionListFragment extends Fragment {
 		getView().findViewById(R.id.empty).setVisibility(View.GONE);
 		getView().findViewById(R.id.list).setVisibility(View.VISIBLE);
 	}
-	
+
+	@Override
+	public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
+		switch(pos) {
+		case 0: //hello world example
+			break;
+		case 1: //printing invoice
+			break;
+		case 2:// printing invoice style
+			break;
+		case 3:// printing invoice images
+			break;
+		}
+	}
+
 }
